5efd349edfc742b2092a00275d2ccf8d50246b6c
vikasprajapat2 <vikasprajapat34118@gmail.com>
2026-05-23 11:58:39 +0530

    setup FastAPI Moduls and PostgreSQl Connection

---
+++ backend/app/models/product.py
@@ -0,0 +1,19 @@
+from sqlalchemy import (Column, Integer, String, DECIMAL, Boolean, ForeignKey,TIMESTAMP)
+from sqlalchemy.orm import relationship
+from sqlalchemy.sql import func
+
+from app.database import Base
+class Product(Base):
+    __tablename__="Products"
+
+    id = Column(Integer, primary_key=True, index=True)
+    produnct_name= Column(String(100), nullable=False)
+    sku=Column(String(100),unique=True)
+    barcode=Column(String(100))
+    category_id= Column(Integer,ForeignKey("categories.id"))
+    selling_price = Column(DECIMAL(10,2))
+    reorder_level= Column(Integer)
+    unit = Column(String(20))
+    is_active=Column( Boolean, default=True)
+    created_at = Column(TIMESTAMP(timezone=True), server_default=func.now())
+    category = relationship('Category',back_populates='products')
