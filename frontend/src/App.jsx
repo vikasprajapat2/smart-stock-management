711479b59a939aa899b904300eafcc12fa1312ad
vikasprajapat2 <vikasprajapat34118@gmail.com>
2026-05-26 17:01:11 +0530

    Added frontend setup with React and Tailwind

---
+++ frontend/src/App.jsx
@@ -0,0 +1,11 @@
+function App() {
+  return (
+    <div className="min-h-screen flex items-center justify-center bg-gray-100">
+      <h1 className="text-5xl font-bold text-blue-600">
+        Smart Stock Management 🚀
+      </h1>
+    </div>
+  )
+}
+
+export default App
